81b4a9d8f8e9d217e288f89965690826cd8d91a3
pipertea <piper.toda@wsu.edu>
2026-03-25 22:36:25 -0700

    chevron

--- recipe-minisite/scripts.js
+++ recipe-minisite/scripts.js
@@ -7,5 +7,13 @@ document.querySelector("navIcon").addEventListener("click", function(e){
     document.querySelector("nav").classList.toggle("clicked");
 });
 
+// remove menu .clicked if icon is closed
+if (iconWasClicked || bodyClicked) {
+    let allMenus = document.querySelectorAll("nav > ul > li");
+    for (const eachMenu of allMenus) {
+        eachMenu.classList.remove("clicked");
+    }
+}
+
 
 });
