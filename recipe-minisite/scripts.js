eac6efee34eea017af9a4507cb7e5a84083081a6
pipertea <piper.toda@wsu.edu>
2026-03-25 23:25:22 -0700

    format css

--- recipe-minisite/scripts.js
+++ recipe-minisite/scripts.js
@@ -1,19 +1,12 @@
 // JS for responsive menu
 
-document.addEventListener("DOMContentLoaded", function () {
+// function to hide/show menu items
+function clickNav(bodyClicked) {
+    let navHandle = document.querySelector("nav");
+    let iconWasClicked = navHandle.classList.contains("clicked");
+    if (!bodyClicked) navHandle.classList.toggle("clicked"); // ...toggle a "clicked" class on the nav
+    else navHandle.classList.remove("clicked");
 
-    // respond to clicks on the menu icon
-    document.querySelector("navIcon").addEventListener("click", function (e) {
-        document.querySelector("nav").classList.toggle("clicked");
-    });
-
-    // function to hide/show menu items
-    function clickNav(bodyClicked) {
-        let navHandle = document.querySelector("nav");
-        let iconWasClicked = navHandle.classList.contains("clicked");
-        if (!bodyClicked) navHandle.classList.toggle("clicked"); // ...toggle a "clicked" class on the nav
-        else navHandle.classList.remove("clicked");
-    }
     // remove menu .clicked if icon is closed
     if (iconWasClicked || bodyClicked) {
         let allMenus = document.querySelectorAll("nav > ul > li");
@@ -21,36 +14,37 @@ document.addEventListener("DOMContentLoaded", function () {
             eachMenu.classList.remove("clicked");
         }
     }
+}
 
-    // load click events after DOM loaded
-    document.addEventListener("DOMContentLoaded", function () {
+// load click events after DOM loaded
+document.addEventListener("DOMContentLoaded", function () {
 
-        // respond to clicks on the menu icon
-        document.querySelector("#navIcon").addEventListener("click", function (e) {
-            clickNav(false);
-        });
+    // respond to clicks on the menu icon
+    document.querySelector("#navIcon").addEventListener("click", function (e) {
+        clickNav(false);
+    });
 
-        // handles to all topline nav items
-        let allMenus = document.querySelectorAll("nav>ul>li");
-        for (const eachMenu of allMenus) {
-            // loop through collection of handles individually
-            eachMenu.addEventListener("click", function (e) {
-                let wasClicked = eachMenu.classList.contains("clicked");
-                let allMenus2 = document.querySelectorAll("nav>ul>li");
-                for (const eachMenu2 of allMenus2) {
-                    eachMenu2.classList.remove("clicked");
-                }
-                if (!wasClicked) {
-                    eachMenu.classList.add("clicked"); // if this is newly clicked, add click class back
-                }
-            });
-        }
-
-        // close nav if someone clicks outside nav
-        document.querySelector("html").addEventListener("click", function (e) {
-            if (!e.target.closest("nav")) {
-                clickNav(true);
+    // handles to all topline nav items
+    let allMenus = document.querySelectorAll("nav>ul>li");
+    for (const eachMenu of allMenus) {
+        // loop through collection of handles individually
+        eachMenu.addEventListener("click", function (e) {
+            let wasClicked = eachMenu.classList.contains("clicked");
+            let allMenus2 = document.querySelectorAll("nav>ul>li");
+            for (const eachMenu2 of allMenus2) {
+                eachMenu2.classList.remove("clicked");
+            }
+            if (!wasClicked) {
+                eachMenu.classList.add("clicked"); // if this is newly clicked, add click class back
             }
         });
+    }
 
+    // close nav if someone clicks outside nav
+    document.querySelector("html").addEventListener("click", function (e) {
+        if (!e.target.closest("nav")) {
+            clickNav(true);
+        }
     });
+
+});
