08fa46cb5b15eaf072995b3bc0bbb33577c923ad
pipertea <piper.toda@wsu.edu>
2026-03-25 22:20:58 -0700

    click

--- recipe-minisite/scripts.js
+++ recipe-minisite/scripts.js
@@ -3,7 +3,7 @@
 document.addEventListener("DOMContentLoaded", function(){
 
 // respond to clicks on the menu icon
-document.querySelector("navIcon").addEventListener("clicked", function(e){
+document.querySelector("navIcon").addEventListener("click", function(e){
     document.querySelector("nav").classList.toggle("clicked");
 });
 
